434e63b1106c079c1745e779a8f398816a78a0fb
gards0052 <joshgardner2000@gmail.com>
2016-01-20 11:57:53 +1100

    Update PvP Support

--- Chess/src/Chess/Board.java
+++ Chess/src/Chess/Board.java
@@ -13,14 +13,28 @@ public class Board {
 
 	private String[][] board = { col0, col1, col2, col3, col4, col5, col6, col7 };
 
-	public void makeMove(int x1, int y1, int x2, int y2, String take) {
-		set(x2, y2, get(x1, y1));
-		set(x1, y1, " ");
+	public void swap(int x1, int y1, int x2, int y2) {
+		String temp = get(x1, y1);
+		set(x1, y1, get(x2, y2));
+		set(x2, y2, temp);
 	}
 
-	public void undoMove(int x1, int y1, int x2, int y2, String take) {
+	public void makeMove(int x1, int y1, int x2, int y2, String take, int turn) {
+		if (turn == 0)
+			flip();
+		set(x2, y2, get(x1, y1));
+		set(x1, y1, " ");
+		if (turn == 0)
+			flip();
+	}
+
+	public void undoMove(int x1, int y1, int x2, int y2, String take, int turn) {
+		if (turn == 0)
+			flip();
 		set(x1, y1, get(x2, y2));
 		set(x2, y2, take);
+		if (turn == 0)
+			flip();
 	}
 
 	public boolean get(int x, int y, String value) {
@@ -35,4 +49,20 @@ public class Board {
 		return board[y][x];
 	}
 
+	public void flip() {
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if (Character.isLowerCase(get(x, y).charAt(0))) {
+					set(x, y, String.valueOf(Character.toUpperCase(get(x, y).charAt(0))));
+				} else if (Character.isUpperCase(get(x, y).charAt(0))) {
+					set(x, y, String.valueOf(Character.toLowerCase(get(x, y).charAt(0))));
+				}
+			}
+		}
+		for (int y = 0; y < 4; y++) {
+			for (int x = 0; x < 8; x++) {
+				swap(x, y, 7 - x, 7 - y);
+			}
+		}
+	}
 }
